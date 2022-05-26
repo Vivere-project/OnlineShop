ea6b9b35472088d876b81c09a2fcd3dd5d1fad70
Valeriu Seremet <valeriu3333@gmail.com>
2022-05-27 00:40:13 +0300

    [UI] Fix base url on prod

--- OnlineShop.Web/src/environments/environment.prod.ts
+++ OnlineShop.Web/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: "https://vivere-dev.azurewebsites.net/api/item",
+  baseUrl: "https://vivere-dev.azurewebsites.net/api",
   imageBaseUrl: "https://vivereimages.blob.core.windows.net/images/Screenshot 2021-07-27 at 06.57.58.png"
 };
