82f05fcfa1ef8f679eb2edef8049c4ba4096aba8
Valeriu Seremet <valeriu3333@gmail.com>
2022-05-30 20:02:44 +0300

    Update EShopBot.cs

--- OnlineShop.Api/OnlineShop.TelegramBot/EShopBot.cs
+++ OnlineShop.Api/OnlineShop.TelegramBot/EShopBot.cs
@@ -15,17 +15,17 @@ namespace OnlineShop.TelegramBot
         
         public static void SendItemsMessage(IEnumerable<Item> items)
         {
-            var timezone = TimeZoneInfo.FindSystemTimeZoneById("Asia/Beirut");
-            var timeStamp = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, timezone);
-            var header = $"Ordered at:{timeStamp.ToString("yyyy MM dd hh:mm:ss")}";
-            var body = string.Join('\n', items.Select(i => i.Name));
-            var mesasge = header+Environment.NewLine+body;
-            Bot.SendTextMessageAsync(ValeriuChatId, mesasge.ToString());
+            // var timezone = TimeZoneInfo.FindSystemTimeZoneById("Asia/Beirut");
+            // var timeStamp = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, timezone);
+            // var header = $"Ordered at:{timeStamp.ToString("yyyy MM dd hh:mm:ss")}";
+            // var body = string.Join('\n', items.Select(i => i.Name));
+            // var mesasge = header+Environment.NewLine+body;
+            // Bot.SendTextMessageAsync(ValeriuChatId, mesasge.ToString());
         }
 
         public static void SendTextMessage(string text)
         {
-            Bot.SendTextMessageAsync(ValeriuChatId, text);
+            // Bot.SendTextMessageAsync(ValeriuChatId, text);
         }
     }
 }
